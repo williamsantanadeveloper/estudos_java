f8e2900236419385442402f11be56f92e9b714b9
William Santana <williamandrade1058@gmail.com>
2024-04-01 15:36:57 -0300

    add ex01

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,10 +1,25 @@
 package curso_programacao;
 
+import java.util.Locale;
+
 public class Main {
 
 	public static void main(String[] args) {
+		String name = "William";
+		int age = 19;
+		double salary = 800;
+		int y = 32;
+		double x = 10.35784;
 		System.out.println("Olá mundo!");
-
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f\n", x);
+		System.out.printf("%.4f\n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.2f\n", x);
+		System.out.printf("%.4f\n", x);
+		System.out.printf("Resultado = %.2f em metros\n", x);
+		System.out.printf("%s tem %d e ganha %.2f reais", name, age, salary);
 	}
 
 }
